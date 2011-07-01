04eeb01feb612101777bb4c99dff3f906068af4f
yalab <rudeboyjet@gmail.com>
2011-07-01 23:15:10 +0900

    Generator use copy_file instead of template.

--- lib/generators/nyoibo/install_generator.rb
+++ lib/generators/nyoibo/install_generator.rb
@@ -11,10 +11,10 @@ module Nyoibo
         template "nyoibo.rb.erb", "config/initializers/nyoibo.rb"
         template "nyoibo_en.yml", "config/locales/nyoibo_en.yml"
         template "nyoibo.js.coffee", "app/assets/javascripts/nyoibo.js.coffee"
-        template "../../../../vendor/html5jp/progress.js", "app/assets/javascripts/progress.js"
-        template "../../../../vendor/web-socket-js/web_socket.js", "app/assets/javascripts/web_socket.js"
-        template "../../../../vendor/web-socket-js/swfobject.js", "app/assets/javascripts/swfobject.js"
-        template "../../../../vendor/web-socket-js/WebSocketMain.swf", "public/WebSocketMain.swf"
+        copy_file "../../../../vendor/html5jp/progress.js", "app/assets/javascripts/progress.js"
+        copy_file "../../../../vendor/web-socket-js/web_socket.js", "app/assets/javascripts/web_socket.js"
+        copy_file "../../../../vendor/web-socket-js/swfobject.js", "app/assets/javascripts/swfobject.js"
+        copy_file "../../../../vendor/web-socket-js/WebSocketMain.swf", "public/WebSocketMain.swf"
       end
     end
   end
