00dbfbfaafbaaab0b95f5a54d972e0412f0f76cf
yalab <rudeboyjet@gmail.com>
2011-07-01 18:31:11 +0900

    install generator add coffee-script file.

--- app/helpers/nyoibo_helper.rb
+++ app/helpers/nyoibo_helper.rb
@@ -2,6 +2,10 @@ module NyoiboHelper
   def ws_form_for(model, options={}, &block)
     options[:html] ||= {}
     options[:html][:id] ||= 'ws-form'
-    form_for(model, options, &block) + content_tag(:div, '', :id => 'ws-progress')
+    output = ''
+    output << javascript_tag("var nyoibo =  #{I18n.t('nyoibo').to_json}")
+    output << form_for(model, options, &block)
+    output << content_tag(:div, '', :id => 'ws-progress')
+    output.html_safe
   end
 end
--- lib/generators/nyoibo/install_generator.rb
+++ lib/generators/nyoibo/install_generator.rb
@@ -5,9 +5,11 @@ module Nyoibo
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
 
-      desc "Creates a Nyoibo initializer and copy locale files to your application."
+      desc "Creates a Nyoibo initializer and copy javascript and locale files to your application."
       def copy_initializer
         template "nyoibo.rb.erb", "config/initializers/nyoibo.rb"
+        template "nyoibo_en.yml", "config/locales/nyoibo_en.yml"
+        template "nyoibo.js.coffee", "app/assets/javascripts/nyoibo.js.coffee"
       end
     end
   end
