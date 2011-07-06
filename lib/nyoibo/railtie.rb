b78f25f324a21abeb4f97352e916311cc6a7320b
yalab <rudeboyjet@gmail.com>
2011-07-06 19:28:37 +0900

    Not running if rails env is test.

--- lib/nyoibo/railtie.rb
+++ lib/nyoibo/railtie.rb
@@ -3,6 +3,6 @@ require "nyoibo"
 class Nyoibo::Railtie < Rails::Railtie
   config.after_initialize do
     ENV["NYOIBO_ENV"] = Rails.env
-    Nyoibo.run
+    Nyoibo.run if Rails.env != 'test'
   end
 end
